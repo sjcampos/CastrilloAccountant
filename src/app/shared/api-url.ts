920318f87edab457484ec45f59b409309678df90
Sebastián José Campos Salas <sjcampossalas@gmail.com>
2021-08-05 14:19:20 -0600

    Fixing routes

--- src/app/shared/api-url.ts
+++ src/app/shared/api-url.ts
@@ -2,4 +2,4 @@
 //ec2-3-17-69-5.us-east-2.compute.amazonaws.com
 
 //export const apiUrl = 'http://localhost:4000/';
-export const apiUrl = 'ec2-3-17-69-5.us-east-2.compute.amazonaws.com/';
+export const apiUrl = 'http://ec2-3-17-69-5.us-east-2.compute.amazonaws.com/';
