df882fc946992d070831b7769887e03b160344fc
Sebastián José Campos Salas <sjcampossalas@gmail.com>
2021-08-05 13:36:54 -0600

    Fixing errors

--- src/app/shared/api-url.ts
+++ src/app/shared/api-url.ts
@@ -1,3 +1,5 @@
 //export const apiUrl = 'https://castrilloaccountantapi.herokuapp.com/';
+//ec2-3-17-69-5.us-east-2.compute.amazonaws.com
 
-export const apiUrl = 'http://localhost:4000/';
+//export const apiUrl = 'http://localhost:4000/';
+export const apiUrl = 'ec2-3-17-69-5.us-east-2.compute.amazonaws.com/';
